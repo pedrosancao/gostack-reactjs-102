912cea8f6d18f334158af1e3061dbc0806ad169d
Pedro Sanção <pedro.ferreira@interaktiv.com.br>
2020-09-03 20:47:31 -0300

    Process CSV upload

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,28 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
+    if (uploadedFiles.length === 0) {
+      return;
+    }
+    const data = new FormData();
+    data.append('file', uploadedFiles[0].file);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    setUploadedFiles(
+      files.map(file => {
+        const { name, size } = file;
+        const readableSize = filesize(size);
+        return { file, name, readableSize };
+      }),
+    );
   }
 
   return (
